ebb14a296e071b019d591eacd61a3b433697e745
Raff <rafka.0312@gmail.com>
2024-10-30 14:17:33 +0700

    Update app.js

--- project-truck-cooling/back-end/app.js
+++ project-truck-cooling/back-end/app.js
@@ -20,7 +20,7 @@ dotenv.config();
 
 // Membuat instance Express
 const app = express();
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors()); // Mengizinkan semua permintaan cross-origin
@@ -385,11 +385,11 @@ pool.connect((err) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-  console.log("DB_USER:", process.env.DB_USER);
-  console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
-  console.log("DB_HOST:", process.env.DB_HOST);
-  console.log("DB_PORT:", process.env.DB_PORT);
-  console.log("DB_NAME:", process.env.DB_NAME);
-});
+//app.listen(port, () => {
+//  console.log(`Server is running on port ${port}`);
+//  console.log("DB_USER:", process.env.DB_USER);
+//  console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
+//  console.log("DB_HOST:", process.env.DB_HOST);
+//  console.log("DB_PORT:", process.env.DB_PORT);
+//  console.log("DB_NAME:", process.env.DB_NAME);
+//});
